461a99e2c8a8652c48a77f9c2062a94f2cf6c887
Emir <sabirov.e.g@edu.mirea.ru>
2025-06-15 17:44:54 +0300

    Теперь при регистрации и логине подтягиваются данные о пользователе: аватарка и логин, в связи с чем изменены файлы DTO и модели для соотвествия ответу с сервера

--- app/src/main/java/com/example/elvo/data/network/models/recipient/RecipientFullDTO.kt
+++ app/src/main/java/com/example/elvo/data/network/models/recipient/RecipientFullDTO.kt
@@ -4,7 +4,16 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class RecipientFullDTO(
+    val name: String,
+    val surname: String,
+    val patronymic: String,
     val fullName: String,
     val phone: String,
+    val city: String,
+    val street: String,
+    val house: Int,
+    val building: String,
+    val flat: Int,
+    val floor: Int,
     val address: String
 )
